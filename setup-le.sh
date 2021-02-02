b444c0edf215846d49631dd203ae5b2a33c188be
Benoit Thieurmel <benoit.thieurmel@datastorm.fr>
2021-02-02 09:16:23 +0100

    add new pem

--- setup-le.sh
+++ setup-le.sh
@@ -5,10 +5,11 @@ WORKDIR="/root/ipa-le"
 
 dnf install letsencrypt -y
 
-ipa-cacert-manage install "$WORKDIR/ca/DSTRootCAX3.pem" -n DSTRootCAX3 -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/isrgrootx1.pem" -n ISRGRootCAX1 -t C,,
 ipa-certupdate -v
 
-ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/lets-encrypt-r3.pem" -n ISRGRootCAR3 -t C,,
 ipa-certupdate -v
+
 
 "$(dirname "$0")/renew-le.sh" "--first-time"
